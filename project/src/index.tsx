9982c21a848d3549a3863855ff5916878eab4d0e
FIROKS <firoks1@gmail.com>
2021-10-04 01:25:43 +0300

    Добавляет компоненты страниц приложения

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const AppSettings = {
+  CARDS_AMOUNT: 8,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App cardsAmount = {AppSettings.CARDS_AMOUNT}/>
   </React.StrictMode>,
   document.getElementById('root'));
